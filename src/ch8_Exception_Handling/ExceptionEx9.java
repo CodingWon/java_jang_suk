c1de556d8c18e73990e46af5ef8adf1181d9505b
kjw <kingcoding2298@gmail.com>
2024-02-05 18:00:27 +0900

    add Exceptio-amEx9_임ê³임의로 ê³임고의로 exception 발생 ì시키는 방법

--- src/ch8_Exception_Handling/ExceptionEx9.java
+++ src/ch8_Exception_Handling/ExceptionEx9.java
@@ -2,6 +2,13 @@ package ch8_Exception_Handling;
 
 public class ExceptionEx9 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        try {
+            //Exception e = new Exception("고의로 발생");
+            //throw e;
+            throw new Exception("고의로 발생시킴");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("프로그램 정상 종료");
     }
 }
